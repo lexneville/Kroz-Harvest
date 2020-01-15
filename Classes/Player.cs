9cfec9d5e312e52ce388128f8b25810365f77e53
Jody Richardson <jody@beeta.co>
2020-01-15 16:34:56 +0000

    Added Mana to player and added enemy class

--- Classes/Player.cs
+++ Classes/Player.cs
@@ -8,17 +8,20 @@ namespace Kroz.Classes
     {
         public string PlayerName { get; set; }
         public int PlayerHealth { get; set; }
+        public int PlayerMana { get; set; }
+
         private List<Items> Inventory = new List<Items>();
         public Player(string PlayerName)
         {
             this.PlayerName = PlayerName;
-            this.PlayerHealth = 100;
+            PlayerHealth = 100;
+            PlayerMana = 100;
         }
         public void AddToInventory(Items item)
         {
             if (item != null)
             { 
-                this.Inventory.Add(item);
+                Inventory.Add(item);
                 Console.WriteLine("You have taken the " + item.ItemName + "! You now have " + GetCount() + " item/s in your inventory.");
             }
         }
@@ -61,7 +64,7 @@ namespace Kroz.Classes
         }
         public int GetCount()
         {
-            return this.Inventory.Count;
+            return Inventory.Count;
         }
     }
 }
